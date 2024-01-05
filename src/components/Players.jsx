84559421a7ff89d6ae6f95fdc5f0fe5777da4d17
Fahid <fahidadris@gmail.com>
2024-01-05 22:42:12 +0500

    2nd commit

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -12,11 +12,11 @@ const Players = ({ score, xPlaying, board, playerXTurn, playerYTurn, gameOver}) 
 
     return (
         <div className='flex w-[25rem] sm:w-[28rem] items-center justify-between h-24 gap-2 mb-12 mt-10'>
-            <div className={`bg-white rounded-lg w-3/6 h-full flex justify-evenly items-center text-3xl sm:text-4xl ${cursor} ${bgX} ${xScoreShadow} shadow-lg`} onClick={playerXTurn}>
+            <div className={`rounded-lg w-3/6 h-full flex justify-evenly items-center text-3xl sm:text-4xl ${cursor} ${bgX} ${xScoreShadow} shadow-lg`} onClick={playerXTurn}>
                 <span>Score X: </span>
                 <span> {xScore}</span>
             </div>
-            <div className={`bg-white rounded-lg w-3/6 h-full flex justify-evenly items-center text-3xl sm:text-4xl ${cursor} ${bgO} ${oScoreShadow} shadow-lg`} onClick={playerYTurn}>
+            <div className={`rounded-lg w-3/6 h-full flex justify-evenly items-center text-3xl sm:text-4xl ${cursor} ${bgO} ${oScoreShadow} shadow-lg`} onClick={playerYTurn}>
                 <span>Score O: </span>
                 <span> {oScore}</span>
             </div>
